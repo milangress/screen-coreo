425ceba522f350baed920ccdec38e5b98b826803
Milan G <dev@milangress.de>
2024-09-22 06:01:15 +0200

    feat: Add ImageBlock.svelte

---
+++ src/routes/window/ImageBlock.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+    import { getAssetUrl } from '$lib/utils';
+
+    export let src: string;
+    let assetSrc: string;
+
+    $: if (src) {
+        (async () => {
+            assetSrc = await getAssetUrl(src);
+        })();
+    }
+</script>
+
+<img {src} alt="" />
+
+<style>
+    img {
+        width: 100vw;
+        height: 100vh;
+        object-fit: cover;
+    }
+</style>
